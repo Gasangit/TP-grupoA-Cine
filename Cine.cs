c37c5d9edf843941f5c8de34a8d3bd926f671015
Gasangit <gasan9git@gmail.com>
2023-04-04 13:15:00 -0300

    Agregar archivos de proyecto.

---
+++ Cine.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_grupoA_Cine
+{
+    internal class Cine
+    {
+
+        public List<Usuario> usuarios { get; set; }
+        public List<Funcion> funciones { get; set; }
+        public List<Sala> salas { get; set; }
+        public List<Pelicula> peliculas { get; set; }
+        public Usuario usuarioActual { get; set; }
+
+
+        public Usuario abmUsuario()
+        {
+
+        }
+
+        public void cargarCredito(int idUsuario, double importe)
+        { 
+        
+        }
+
+        public void comprarEntrada(int idUsuario, int idFuncion, int cantidad)
+        { 
+        
+        }
+
+        public void devolverEntrada(int idUsuario, int cantidad)
+        { 
+        
+        }
+
+        public void iniciarSesion(string mail, string password) 
+        {
+        
+        }
+
+        public void cerrarSesion()
+        { 
+        
+        }
+
+        /*  recordar que las listas se tienen que devolver (return) con el método ToList() para no
+            devolver la lista original y que la misma no sea modificada.    */
+        public Funcion mostrarFunciones() 
+        {
+            return funciones.ToList();
+        }
+
+        /*  recordar que las listas se tienen que devolver (return) con el método ToList() para no
+            devolver la lista original y que la misma no sea modificada.    */
+        public Sala mostrarSalas()
+        {
+            return salas.ToList();
+        }
+
+        /*  recordar que las listas se tienen que devolver (return) con el método ToList() para no
+            devolver la lista original y que la misma no sea modificada.    */
+        public Pelicula mostrarPeliculas()
+        {
+            return peliculas.ToList();
+        }
+
+        public Funcion buscarFuncion(string ubicacion, Date fecha, double costo, string pelicula)
+        { 
+        
+        }
+
+
+    }
+}
